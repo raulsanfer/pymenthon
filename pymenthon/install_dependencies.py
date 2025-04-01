b29243663ef80fdceb668a126930b24a7729e37e
raulsan <sanchezraul77@gmail.com>
2025-04-01 21:56:53 +0200

    revision install dependencies

--- pymenthon/install_dependencies.py
+++ pymenthon/install_dependencies.py
@@ -1,6 +1,6 @@
 import subprocess
 import sys
 
-def install_dependencies(requirements_path):
-    print(f"Instalando dependencias desde {requirements_path}...")
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", requirements_path])
+def install_dependencies(requirements_file):
+    print(f"Instalando dependencias desde {requirements_file}...")
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", requirements_file])
